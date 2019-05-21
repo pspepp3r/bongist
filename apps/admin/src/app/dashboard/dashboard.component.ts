38803b8186028b4ca337940a2625f1b57ee747cf
Samuel Ogu <hello@samuelogu.com>
2019-05-21 22:13:24 +0100

    i

--- apps/admin/src/app/dashboard/dashboard.component.ts
+++ apps/admin/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {log} from "util";
 
 @Component({
   selector: 'bongist-dashboard',
@@ -10,6 +11,7 @@ export class DashboardComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    console.log('good')
   }
 
 }
