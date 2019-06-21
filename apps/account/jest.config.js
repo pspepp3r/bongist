0289f462fb4fdeaadcc2424e18461782bb258819
Samuel Ogu <hello@samuelogu.com>
2019-06-21 23:07:48 +0100

    revamp

--- apps/account/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'account',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/account/',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
-};
