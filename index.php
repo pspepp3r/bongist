1d533637c8d33232bd6ac398b55ae37e47190ef5
Samuel Ogu <hello@samuelogu.com>
2019-06-21 23:27:34 +0100

    init php

--- index.php
+++ index.php
@@ -67,7 +67,7 @@ foreach($routes as $route)
 Now, $routes will contain all the routes. $routes[0] will correspond to first route. For e.g. in above example $routes[0] is search, $routes[1] is book and $routes[2] is fitzgerald
 */
 
-$pages = array("home", "shop", "lookbook", "admin", "contact", "account", "login", "orders", "signup", "cart", "checkout", "contact", "about", "account");
+$pages = array("home", "admin", "contact", "account");
 
 if ($routes[1] != '') {
 
---
+++ views/admin.php
@@ -0,0 +1,50 @@
+<?php
+
+$pages = array("login", "password", "logout", "customers", "dashboard");
+
+if ($routes[2] != 'admin') {
+
+    if (in_array($routes[2], $pages)) {
+
+        $page = $routes[2];
+
+    } else {
+
+        $page = 'dashboard';
+
+    }
+
+} else {
+    $page = 'dashboard';
+}
+
+if ($page == 'login') {
+    if (isset($_SESSION['logged_staff'])) {
+        $page = 'dashboard';
+    }
+    require('views/admin/'.$page.'.php');
+} else{
+
+    if (!isset($_SESSION['logged_staff'])) {
+        $page = 'login';
+    }else{
+      $email = $_SESSION['logged_staff'];
+        $account = $db->query("SELECT * FROM staff WHERE email = :email", array('email' => $email), false);
+
+        if ($account) {
+            $staff_id = $account['id'];
+            $staff_role = $account['role'];
+        }else{
+            unset($_SESSION['logged_staff']);
+            $page = 'login';
+        }
+    }
+
+    if ($page == 'login') {
+        require('views/admin/'.$page.'.php');
+    }else {
+        require('views/admin/partials/header.php');
+        require('views/admin/'.$page.'.php');
+        require('views/admin/partials/footer.php');
+    }
+}
